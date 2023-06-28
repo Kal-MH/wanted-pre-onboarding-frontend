725c8392f19f5f1dfe7c51f1dcbab389006f40a5
Kal-MH <rkfalgus713@gmail.com>
2023-06-28 20:58:39 +0900

    refactor: login, signup page - set state(email, password)

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,16 +1,29 @@
 import styled from "@emotion/styled";
+import { ChangeEvent, useState } from "react";
 
 import { Button, Input } from "../components/base";
 
 const Signin = () => {
+  const [inputValue, setInputValue] = useState({ email: "", password: "" });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+
   return (
     <Container>
-      <Input data-testid='email-input' name='email' placeholder='email' />
+      <Input
+        data-testid='email-input'
+        name='email'
+        placeholder='email'
+        onChange={handleInputChange}
+      />
       <Input
         data-testid='password-input'
         name='password'
         type='password'
         placeholder='password'
+        onChange={handleInputChange}
       />
       <Button data-testid='signin-button'>Sign In</Button>
     </Container>
