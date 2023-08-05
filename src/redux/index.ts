842bf0ba9630abe0713809c2db48a98436a51c57
Kal-MH <rkfalgus713@gmail.com>
2023-08-05 21:57:48 +0900

    feature: set logger middleware

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,9 +1,14 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 
 import { todoReducer } from "./todos";
 
 const rootReducer = combineReducers({ todoReducer });
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export type RootType = ReturnType<typeof rootReducer>;
